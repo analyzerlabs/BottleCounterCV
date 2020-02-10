e33971e2ce6138fdf10676aa7de6ba3d2c7f2a24
analyzerlabs <miguelquispecastro@uni.pe>
2020-02-10 02:20:30 -0500

    xD

--- InterfazLcd.py
+++ InterfazLcd.py
@@ -8,12 +8,12 @@ class InterfazLCD:
     def __init__(self,s):
         serie = s
         # Raspberry Pi pin configuration:
-        lcd_rs        = 27  # Note this might need to be changed to 21 for older revision Pi's.
-        lcd_en        = 22
-        lcd_d4        = 25
-        lcd_d5        = 24
-        lcd_d6        = 23
-        lcd_d7        = 18
+        lcd_rs        = 26  # Note this might need to be changed to 21 for older revision Pi's.
+        lcd_en        = 19
+        lcd_d4        = 13
+        lcd_d5        = 6
+        lcd_d6        = 5
+        lcd_d7        = 11
         lcd_backlight = 4
         # Define LCD column and row size for 16x2 LCD.
         lcd_columns = 16
@@ -35,9 +35,27 @@ class InterfazLCD:
     def addCounter(self):
         self.counter = self.counter + 1
 
-    def lcd.get_Threshold(self):
+    def get_Threshold(self):
         return (self.minT,self.maxT)
-    
+
+    def show_Thershold(self): 
+        self.lcd.clear()
+        self.lcd.set_cursor(0,0)
+        self.lcd.message('minThr = ')
+        self.lcd.set_cursor(10,1)
+        self.lcd.message(str(self.minT))
+        self.lcd.set_cursor(0,1)
+        self.lcd.message('maxThr = ')
+        self.lcd.set_cursor(10,1)
+        self.lcd.message(str(self.maxT))
+
+    def menu(self):
+        self.lcd.clear()
+        self.lcd.set_cursor(0,0)
+        self.lcd.message(' 1.Init 2.Reset ')
+        self.lcd.set_cursor(0,1)
+        self.lcd.message('maxThr = ')
+        
         
 
 # Demo showing the cursor.
