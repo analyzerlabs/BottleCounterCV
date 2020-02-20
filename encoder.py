2ce00e2b39d1d1c8592f5b681fe46206a0625a58
analyzerlabs <miguelquispecastro@uni.pe>
2020-02-20 00:23:05 -0500

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -8,12 +8,12 @@ encoder_buttom  = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+def encoder_interrupt():
+    print("CLK,DT "+str(GPIO.input(encoder_clock))+","+str(GPIO.input(encoder_data)))
+
 GPIO.setup(encoder_clock, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(encoder_data, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(encoder_clock, GPIO.BOTH, callback=encoder_interrupt)  # add rising edge detection on a channel
-
-def encoder_interrupt():
-    print("CLK,DT "+str(GPIO.input(encoder_clock))+","+str(GPIO.input(encoder_data)))
 
 try:
     while True:
