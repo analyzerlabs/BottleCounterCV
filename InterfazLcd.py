f6890c9a2867a0ec870ea2186e9c2a13d48f867f
analyzerlabs <miguelquispecastro@uni.pe>
2020-03-02 17:21:29 -0500

    Update InterfazLcd.py

--- InterfazLcd.py
+++ InterfazLcd.py
@@ -196,16 +196,16 @@ class InterfazLCD:
         # Set character / data bit.
         self._gpio.output(self._rs, char_mode)
         # Write upper 4 bits.
-        self._gpio.output_pins({ self._d4: ((value >> 4) & 1) > 0,
-                                 self._d5: ((value >> 5) & 1) > 0,
-                                 self._d6: ((value >> 6) & 1) > 0,
-                                 self._d7: ((value >> 7) & 1) > 0 })
+        GPIO.output(self._d4, ((value >> 4) & 1) > 0)
+        GPIO.output(self._d5, ((value >> 5) & 1) > 0)
+        GPIO.output(self._d6, ((value >> 6) & 1) > 0)
+        GPIO.output(self._d7, ((value >> 7) & 1) > 0)
         self._pulse_enable()
         # Write lower 4 bits.
-        self._gpio.output_pins({ self._d4: (value        & 1) > 0,
-                                 self._d5: ((value >> 1) & 1) > 0,
-                                 self._d6: ((value >> 2) & 1) > 0,
-                                 self._d7: ((value >> 3) & 1) > 0 })
+        GPIO.output(self._d4, (value        & 1) > 0)
+        GPIO.output(self._d5, ((value >> 1) & 1) > 0)
+        GPIO.output(self._d6, ((value >> 2) & 1) > 0)
+        GPIO.output(self._d7, ((value >> 3) & 1) > 0)
         self._pulse_enable()
 
     def create_char(self, location, pattern):
