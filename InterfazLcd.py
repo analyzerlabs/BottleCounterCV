0d2ec498afe7b8ca3f6b61bdca993e3f63982b55
analyzerlabs <miguelquispecastro@uni.pe>
2020-03-02 18:40:57 -0500

    lcd

--- InterfazLcd.py
+++ InterfazLcd.py
@@ -3,7 +3,28 @@ from datetime import date
 import datetime
 import RPi.GPIO as GPIO 
 import time
+LCD_RS = 25
+LCD_E = 24
+LCD_D4 = 23
+LCD_D5 = 17
+LCD_D6 = 27
+LCD_D7 = 22
+LED_ON = 15
 
+# Define some device constants
+
+LCD_WIDTH = 0x10  # Maximum characters per line
+LCD_CHR = True
+LCD_CMD = False
+
+LCD_LINE_1 = 0x80  # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0  # LCD RAM address for the 2nd line
+LCD_LINE_3 = 0x94  # LCD RAM address for the 3rd line
+LCD_LINE_4 = 0xD4  # LCD RAM address for the 4th line 
+# Timing constants
+
+E_PULSE = 0.00005
+E_DELAY = 0.00005
 class InterfazLCD:
     counter = 0
     serie = 0
@@ -11,229 +32,164 @@ class InterfazLCD:
     maxT = 30
     mintentative = -36
     maxtentative = 36
-    # DEFINICIONES PARA ASIGNACION DE PINES
-    LCD_CLEARDISPLAY        = 0x01
-    LCD_RETURNHOME          = 0x02
-    LCD_ENTRYMODESET        = 0x04
-    LCD_DISPLAYCONTROL      = 0x08
-    LCD_CURSORSHIFT         = 0x10
-    LCD_FUNCTIONSET         = 0x20
-    LCD_SETCGRAMADDR        = 0x40
-    LCD_SETDDRAMADDR        = 0x80
-    # Entry flags
-    LCD_ENTRYRIGHT          = 0x00
-    LCD_ENTRYLEFT           = 0x02
-    LCD_ENTRYSHIFTINCREMENT = 0x01
-    LCD_ENTRYSHIFTDECREMENT = 0x00
-    # Control flags
-    LCD_DISPLAYON           = 0x04
-    LCD_DISPLAYOFF          = 0x00
-    LCD_CURSORON            = 0x02
-    LCD_CURSOROFF           = 0x00
-    LCD_BLINKON             = 0x01
-    LCD_BLINKOFF            = 0x00
-    # Move flags
-    LCD_DISPLAYMOVE         = 0x08
-    LCD_CURSORMOVE          = 0x00
-    LCD_MOVERIGHT           = 0x04
-    LCD_MOVELEFT            = 0x00
-    # Function set flags
-    LCD_8BITMODE            = 0x10
-    LCD_4BITMODE            = 0x00
-    LCD_2LINE               = 0x08
-    LCD_1LINE               = 0x00
-    LCD_5x10DOTS            = 0x04
-    LCD_5x8DOTS             = 0x00
-    # Offset for up to 4 rows.
-    LCD_ROW_OFFSETS         = (0x00, 0x40, 0x14, 0x54)
-    # Char LCD plate GPIO numbers.
-    LCD_PLATE_RS            = 20
-    LCD_PLATE_RW            = 14
-    LCD_PLATE_EN            = 21
-    LCD_PLATE_D4            = 18
-    LCD_PLATE_D5            = 23
-    LCD_PLATE_D6            = 24
-    LCD_PLATE_D7            = 25
-    LCD_PLATE_RED           = 6
-    LCD_PLATE_GREEN         = 7
-    LCD_PLATE_BLUE          = 8
-    # Char LCD plate button names.
-    SELECT                  = 0
-    RIGHT                   = 1
-    DOWN                    = 2
-    UP                      = 3
-    LEFT                    = 4
-    # Char LCD backpack GPIO numbers.
-    LCD_BACKPACK_RS         = 1
-    LCD_BACKPACK_EN         = 2
-    LCD_BACKPACK_D4         = 3
-    LCD_BACKPACK_D5         = 4
-    LCD_BACKPACK_D6         = 5
-    LCD_BACKPACK_D7         = 6
-    LCD_BACKPACK_LITE       = 7
-    GPIO = GPIO
+    
     def __init__(self,s):
         serie = s
         # DEFINIR GPIO COMO SALIDA PARA USAR LA LCD
-        self.GPIO.setmode(self.GPIO.BCM) # USAR LA NOMENCLATURA BCM (HARDWARE)
-        self.GPIO.setwarnings(False)
-        self.lcd_init(self.LCD_PLATE_RS,self.LCD_PLATE_EN,self.LCD_PLATE_D4,self.LCD_PLATE_D5,
-        self.LCD_PLATE_D6,self.LCD_PLATE_D7,16,2)
+        GPIO.setwarnings(False)
+
+        # Initialise display
+
+        self.lcd_init()
 
         # initialize encoder
         #self.button_r = Button(encoder_buttom)
-    def lcd_init(self, rs, en, d4, d5, d6, d7, cols, lines, backlight=None):
-        self._cols = cols
-        self._lines = lines
-        # Save GPIO state and pin numberS
-        self._rs = rs
-        self._en = en
-        self._d4 = d4
-        self._d5 = d5
-        self._d6 = d6
-        self._d7 = d7
-        for pin in (rs, en, d4, d5, d6, d7):
-            self.GPIO.setup(pin, self.GPIO.OUT)
-         # Initialize the display.
-        self.write8(0x33)
-        self.write8(0x32)
-        # Initialize display control, function, and mode registers.
-        self.displaycontrol = self.LCD_DISPLAYON | self.LCD_CURSOROFF | self.LCD_BLINKOFF
-        self.displayfunction = self.LCD_4BITMODE | self.LCD_1LINE | self.LCD_2LINE | self.LCD_5x8DOTS
-        self.displaymode = self.LCD_ENTRYLEFT | self.LCD_ENTRYSHIFTDECREMENT
-        # Write registers.
-        self.write8(self.LCD_DISPLAYCONTROL | self.displaycontrol)
-        self.write8(self.LCD_FUNCTIONSET | self.displayfunction)
-        self.write8(self.LCD_ENTRYMODESET | self.displaymode)  # set the entry mode
-        self.lcd_clear()
+    def lcd_init(self):
+        GPIO.setmode(GPIO.BCM)  # Use BCM GPIO numbers
+        GPIO.setup(LCD_E, GPIO.OUT)  # E
+        GPIO.setup(LCD_RS, GPIO.OUT)  # RS
+        GPIO.setup(LCD_D4, GPIO.OUT)  # DB4
+        GPIO.setup(LCD_D5, GPIO.OUT)  # DB5
+        GPIO.setup(LCD_D6, GPIO.OUT)  # DB6
+        GPIO.setup(LCD_D7, GPIO.OUT)  # DB7
+        GPIO.setup(LED_ON, GPIO.OUT)  # Backlight enable
 
-    def lcd_clear(self):
-        self.write8(self.LCD_CLEARDISPLAY)  # command to clear display
-        self._delay_microseconds(3000)  # 3000 microsecond sleep, clearing the display takes a long time
+        # Initialise display
 
-    def lcd_set_cursor(self, col, row):
-        """Move the cursor to an explicit column and row position."""
-        # Clamp row to the last row of the display.
-        if row > self._lines:
-            row = self._lines - 1
-        # Set location.
-        self.write8(self.LCD_SETDDRAMADDR | (col + self.LCD_ROW_OFFSETS[row]))
+        self.lcd_byte(0x33, LCD_CMD)
+        self.lcd_byte(0x32, LCD_CMD)
+        self.lcd_byte(0x28, LCD_CMD)
+        self.lcd_byte(0x0C, LCD_CMD)
+        self.lcd_byte(0x06, LCD_CMD)
+        self.lcd_byte(0x01, LCD_CMD)
 
-    def enable_display(self, enable):
-        """Enable or disable the display.  Set enable to True to enable."""
-        if enable:
-            self.displaycontrol |= self.LCD_DISPLAYON
-        else:
-            self.displaycontrol &= ~self.LCD_DISPLAYON
-        self.write8(self.LCD_DISPLAYCONTROL | self.displaycontrol)
+    def message(self, message, style=1,speed=1):
+        # Auto splits, not perfect for clock
+        # Send string to display
+        # style=1 Left justified
+        # style=2 Centred
+        # style=3 Right justified
+        # style=4 typing
 
-    def show_cursor(self, show):
-        """Show or hide the cursor.  Cursor is shown if show is True."""
-        if show:
-            self.displaycontrol |= self.LCD_CURSORON
-        else:
-            self.displaycontrol &= ~self.LCD_CURSORON
-        self.write8(self.LCD_DISPLAYCONTROL | self.displaycontrol)
+        msgs = message.split('\n')
+        for (idx, msg) in enumerate(msgs):
+            if idx == 0:
+                self.lcd_byte(LCD_LINE_1, LCD_CMD)
+            elif idx == 0x01:
+                self.lcd_byte(LCD_LINE_2, LCD_CMD)
+            if style != 4:
+                self.lcd_string(msg, style)
+            elif style == 4:
+                self.type_string(msg, speed)
 
-    def blink(self, blink):
-        """Turn on or off cursor blinking.  Set blink to True to enable blinking."""
-        if blink:
-            self.displaycontrol |= self.LCD_BLINKON
-        else:
-            self.displaycontrol &= ~self.LCD_BLINKON
-        self.write8(self.LCD_DISPLAYCONTROL | self.displaycontrol)
+    def type_string(self, message, speed=1, style=1):
 
-    def move_left(self):
-        """Move display left one position."""
-        self.write8(self.LCD_CURSORSHIFT | self.LCD_DISPLAYMOVE | self.LCD_MOVELEFT)
+        # Send string to display
+        # style=1 Left justified
+        # style=2 Centred
+        # style=3 Right justified
 
-    def move_right(self):
-        """Move display right one position."""
-        self.write8(self.LCD_CURSORSHIFT | self.LCD_DISPLAYMOVE | self.LCD_MOVERIGHT)
+        if style == 0x01:
+            message = message.ljust(LCD_WIDTH, ' ')
+        elif style == 0x02:
+            message = message.center(LCD_WIDTH, ' ')
+        elif style == 3:
+            message = message.rjust(LCD_WIDTH, ' ')
 
-    def set_left_to_right(self):
-        """Set text direction left to right."""
-        self.displaymode |= self.LCD_ENTRYLEFT
-        self.write8(self.LCD_ENTRYMODESET | self.displaymode)
+        for i in range(LCD_WIDTH):
+            self.lcd_byte(ord(message[i]), LCD_CHR)
+            if message[i] != " ":
+                time.sleep(speed)
 
-    def set_right_to_left(self):
-        """Set text direction right to left."""
-        self.displaymode &= ~self.LCD_ENTRYLEFT
-        self.write8(self.LCD_ENTRYMODESET | self.displaymode)
+    def clear(self):
+        self.lcd_byte(0x06, LCD_CMD)
+        self.lcd_byte(0x01, LCD_CMD)
+        time.sleep(0.45)
 
-    def autoscroll(self, autoscroll):
-        """Autoscroll will 'right justify' text from the cursor if set True,
-        otherwise it will 'left justify' the text.
-        """
-        if autoscroll:
-            self.displaymode |= self.LCD_ENTRYSHIFTINCREMENT
-        else:
-            self.displaymode &= ~self.LCD_ENTRYSHIFTINCREMENT
-        self.write8(self.LCD_ENTRYMODESET | self.displaymode)
+    def write_line1(self, message, style):
+        self.lcd_byte(LCD_LINE_1, LCD_CMD)
+        self.lcd_string(message, style)
 
-    def lcd_message(self, text):
-        """Write text to display.  Note that text can include newlines."""
-        line = 0
-        # Iterate through each character.
-        for char in text:
-            # Advance to next line if character is a new line.
-            if char == '\n':
-                line += 1
-                # Move to left or right side depending on text direction.
-                col = 0 if self.displaymode & self.LCD_ENTRYLEFT > 0 else self._cols-1
-                self.set_cursor(col, line)
-            # Write the character to the display.
-            else:
-                self.write8(ord(char), True)
+    def write_line2(self, message, style):
+        self.lcd_byte(LCD_LINE_2, LCD_CMD)
+        self.lcd_string(message, style)
 
-    def write8(self, value, char_mode=False):
-        """Write 8-bit value in character or data mode.  Value should be an int
-        value from 0-255, and char_mode is True if character data or False if
-        non-character data (default).
-        """
-        # One millisecond delay to prevent writing too quickly.
-        self._delay_microseconds(1000)
-        # Set character / data bit.
-        self.GPIO.output(self._rs, char_mode)
-        # Write upper 4 bits.
-        self.GPIO.output(self._d4, ((value >> 4) & 1) > 0)
-        self.GPIO.output(self._d5, ((value >> 5) & 1) > 0)
-        self.GPIO.output(self._d6, ((value >> 6) & 1) > 0)
-        self.GPIO.output(self._d7, ((value >> 7) & 1) > 0)
-        self._pulse_enable()
-        # Write lower 4 bits.
-        self.GPIO.output(self._d4, (value        & 1) > 0)
-        self.GPIO.output(self._d5, ((value >> 1) & 1) > 0)
-        self.GPIO.output(self._d6, ((value >> 2) & 1) > 0)
-        self.GPIO.output(self._d7, ((value >> 3) & 1) > 0 )
-        self._pulse_enable()
-    def _pulse_enable(self):
-        # Pulse the clock enable line off, on, off to send command.
-        self.GPIO.output(self._en, False)
-        self._delay_microseconds(1)       # 1 microsecond pause - enable pulse must be > 450ns
-        self.GPIO.output(self._en, True)
-        self._delay_microseconds(1)       # 1 microsecond pause - enable pulse must be > 450ns
-        self.GPIO.output(self._en, False)
-        self._delay_microseconds(1)       # commands need > 37us to settle
+    def set_line1(self):
+        self.lcd_byte(LCD_LINE_1, LCD_CMD)
 
-    def create_char(self, location, pattern):
-        """Fill one of the first 8 CGRAM locations with custom characters.
-        The location parameter should be between 0 and 7 and pattern should
-        provide an array of 8 bytes containing the pattern. E.g. you can easyly
-        design your custom character at http://www.quinapalus.com/hd44780udg.html
-        To show your custom character use eg. lcd.message('\x01')
-        """
-        # only position 0..7 are allowed
-        location &= 0x7
-        self.write8(self.LCD_SETCGRAMADDR | (location << 3))
-        for i in range(8):
-            self.write8(pattern[i], char_mode=True)
+    def set_line2(self):
+        self.lcd_byte(LCD_LINE_2, LCD_CMD)
 
-    def _delay_microseconds(self, microseconds):
-        # Busy wait in loop because delays are generally very short (few microseconds).
-        end = time.time() + (microseconds/1000000.0)
-        while time.time() < end:
-            pass
+    def lcd_string(self, message, style):
+        # Send string to display
+        # style=1 Left justified
+        # style=2 Centred
+        # style=3 Right justified
+
+        if style == 0x01:
+            message = message.ljust(LCD_WIDTH, ' ')
+        elif style == 0x02:
+            message = message.center(LCD_WIDTH, ' ')
+        elif style == 3:
+            message = message.rjust(LCD_WIDTH, ' ')
+
+        for i in range(LCD_WIDTH):
+            self.lcd_byte(ord(message[i]), LCD_CHR)
+
+    def lcd_byte(self, bits, mode):
+
+        # Send byte to data pins
+        # bits = data
+        # mode = True  for character
+        #        False for command
+
+        GPIO.output(LCD_RS, mode)  # RS
+
+        # High bits
+
+        GPIO.output(LCD_D4, False)
+        GPIO.output(LCD_D5, False)
+        GPIO.output(LCD_D6, False)
+        GPIO.output(LCD_D7, False)
+        if bits & 0x10 == 0x10:
+            GPIO.output(LCD_D4, True)
+        if bits & 0x20 == 0x20:
+            GPIO.output(LCD_D5, True)
+        if bits & 0x40 == 0x40:
+            GPIO.output(LCD_D6, True)
+        if bits & 0x80 == 0x80:
+            GPIO.output(LCD_D7, True)
+
+            # Toggle 'Enable' pin
+
+        time.sleep(E_DELAY)
+        GPIO.output(LCD_E, True)
+        time.sleep(E_PULSE)
+        GPIO.output(LCD_E, False)
+        time.sleep(E_DELAY)
+
+        # Low bits
+
+        GPIO.output(LCD_D4, False)
+        GPIO.output(LCD_D5, False)
+        GPIO.output(LCD_D6, False)
+        GPIO.output(LCD_D7, False)
+        if bits & 0x01 == 0x01:
+            GPIO.output(LCD_D4, True)
+        if bits & 0x02 == 0x02:
+            GPIO.output(LCD_D5, True)
+        if bits & 0x04 == 0x04:
+            GPIO.output(LCD_D6, True)
+        if bits & 0x08 == 0x08:
+            GPIO.output(LCD_D7, True)
+
+            # Toggle 'Enable' pin
+
+        time.sleep(E_DELAY)
+        GPIO.output(LCD_E, True)
+        time.sleep(E_PULSE)
+        GPIO.output(LCD_E, False)
+        time.sleep(E_DELAY)
     
     def showCounter(self):
         self.lcd_clear()
@@ -251,7 +207,7 @@ class InterfazLCD:
         return (self.minT,self.maxT)
 
     def show_Thershold(self): 
-        self.lcd.clear()
+        self.lcd_clear()
         self.lcd_set_cursor(0,0)
         self.lcd_message('minThr = ')
         self.lcd_set_cursor(10,1)
@@ -265,7 +221,7 @@ class InterfazLCD:
         self.counter = 0;
     
     def showMax(self,val1,val2):
-        self.lcd.clear()
+        self.lcd_clear()
         self.lcd_set_cursor(0,0)
         text = 'Max: ' +str(val1)
         self.lcd_message(text)
@@ -278,7 +234,7 @@ class InterfazLCD:
         self.mintentative = mini
 
     def showTentative(self):
-        self.lcd.clear()
+        self.lcd_clear()
         self.lcd_set_cursor(0,0)
         text = 'Max: ' +str(self.maxtentative)
         self.lcd_message(text)
@@ -292,7 +248,7 @@ class InterfazLCD:
         self.maxT = self.maxtentative*.85
 
     def menu(self):
-        self.lcd.clear()
+        self.lcd_clear()
         self.lcd_set_cursor(0,0)
         self.lcd_message(' 1. Initialize ')
         self.lcd_set_cursor(0,1)
