9d7c71a0d4183ce092bab2b99f4b55decec28781
analyzerlabs <miguelquispecastro@uni.pe>
2020-03-13 17:33:20 -0500

    Update InterfazLcd.py

--- InterfazLcd.py
+++ InterfazLcd.py
@@ -28,8 +28,6 @@ class InterfazLCD:
         serie = s
         # DEFINIR GPIO COMO SALIDA PARA USAR LA LCD
         GPIO.setwarnings(False)
-        GPIO.setup(lcd_rs,1)
-        GPIO.setup(lcd_en,1)
         
         # initialize encoder
         #self.button_r = Button(encoder_buttom)
